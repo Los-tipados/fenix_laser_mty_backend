79b7a6a97c2eface48913d92fe94c02d6e8b8363
erik-perez-1 <erikp2913@gmail.com>
2026-02-13 16:11:26 -0600

    feat: creacion de las variables de instancia

--- src/main/java/com/fenix_laser_mty/backend/modelos/Usuario.java
+++ src/main/java/com/fenix_laser_mty/backend/modelos/Usuario.java
@@ -1,4 +1,37 @@
 package com.fenix_laser_mty.backend.modelos;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table (name ="usuarios")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id_usuario")
+    private Integer idUsuario;
+
+    @Column (name = "id_nombre")
+    private String nombre;
+
+    @Column (name="correo", unique = true, length = 100)
+    private String correo;
+
+    @Column(name = "telefono", nullable = false, length = 15)
+    private String telefono;
+
+    @Column (name= "password", nullable = false, length = 255)
+    private String password;
+
+    @Column (name ="rol", length = 20)
+    private  String rol="cliente";
+
+    @Column (name ="fecha_registro", updatable = false)
+    private LocalDateTime fechaRegistro= LocalDateTime.now();
+
+
+
 }
