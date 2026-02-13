c9285a9466d96feba627cc78034425daca0107e8
Gerardo Torres <gerardo.torres4590@gmail.com>
2026-02-12 22:45:39 -0600

    feat: Se crea entidad categorias

--- src/main/java/com/fenix_laser_mty/backend/modelos/Categoria.java
+++ src/main/java/com/fenix_laser_mty/backend/modelos/Categoria.java
@@ -1,4 +1,63 @@
 package com.fenix_laser_mty.backend.modelos;
 
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "categorias")
 public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria", nullable = false, unique = true)
+    private Integer idCategoria;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    public Categoria(Integer idCategoria, String nombre) {
+        this.idCategoria = idCategoria;
+        this.nombre = nombre;
+    }
+
+    public Categoria() {
+    }
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "idCategoria=" + idCategoria +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Categoria categoria)) return false;
+        return Objects.equals(idCategoria, categoria.idCategoria) && Objects.equals(nombre, categoria.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idCategoria, nombre);
+    }
+
+
 }
