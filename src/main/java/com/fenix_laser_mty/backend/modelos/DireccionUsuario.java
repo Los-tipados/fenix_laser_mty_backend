c9775b4cd9730b4e13abe7421f77f4c217f3d4e4
Alexis Castillo <uclastle9112342@gmail.com>
2026-02-12 21:12:46 -0600

    feat: crear entidad DireccionUsuario y mapeo de tabla

--- src/main/java/com/fenix_laser_mty/backend/modelos/DireccionUsuario.java
+++ src/main/java/com/fenix_laser_mty/backend/modelos/DireccionUsuario.java
@@ -1,4 +1,15 @@
 package com.fenix_laser_mty.backend.modelos;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "direccion_usuario")
 public class DireccionUsuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_direccion")
+    private Long id_direccion;
+    
+
+
 }
