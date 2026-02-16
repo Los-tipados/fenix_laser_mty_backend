fcee8c65f360ac828c66503680bec2c38a293dd4
erik-perez-1 <erikp2913@gmail.com>
2026-02-16 11:06:08 -0600

    fix: se quito el valor default de la variable rol

--- src/main/java/com/fenix_laser_mty/backend/modelos/Usuario.java
+++ src/main/java/com/fenix_laser_mty/backend/modelos/Usuario.java
@@ -28,7 +28,7 @@ public class Usuario {
     private String password;
 
     @Column (name ="rol", length = 20)
-    private  String rol="cliente"; // ojito
+    private  String rol; // ojito
 
     @Column (name ="fecha_registro", updatable = false)
     private LocalDateTime fechaRegistro= LocalDateTime.now();
