49d5265b4ca7e3ff4f2f90045fcc094798aeee47
erik-perez-1 <erikp2913@gmail.com>
2026-02-16 15:32:09 -0600

    feat: se creo el repository de Categoria

--- src/main/java/com/fenix_laser_mty/backend/repositorios/CategoriaRepository.java
+++ src/main/java/com/fenix_laser_mty/backend/repositorios/CategoriaRepository.java
@@ -1,5 +1,12 @@
 package com.fenix_laser_mty.backend.repositorios;
 
-public interface CategoriaRepository {
+import com.fenix_laser_mty.backend.modelos.Categoria;
+import com.fenix_laser_mty.backend.modelos.Producto;
+import com.fenix_laser_mty.backend.modelos.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository <Categoria, Integer> {
+
+    Categoria findByCategory(String username);
 
 }
