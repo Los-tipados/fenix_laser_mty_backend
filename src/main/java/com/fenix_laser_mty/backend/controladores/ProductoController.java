fb8c2ed5e3b58f4fed959d2ddc59862a71485597
erik-perez-1 <erikp2913@gmail.com>
2026-02-17 16:09:48 -0600

    feat: se crean los mapean los /products y /new-product7

--- src/main/java/com/fenix_laser_mty/backend/controladores/ProductoController.java
+++ src/main/java/com/fenix_laser_mty/backend/controladores/ProductoController.java
@@ -1,14 +1,38 @@
 package com.fenix_laser_mty.backend.controladores;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fenix_laser_mty.backend.modelos.Producto;
+import com.fenix_laser_mty.backend.servicios.ProductoServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
 @CrossOrigin(origins = "*") //se habikita todo a nivel clase o metodos
 
 public class ProductoController {
+
+    private final ProductoServicio productoServicio;
+    @Autowired
+    public ProductoController ( ProductoServicio productoServicio){
+        this.productoServicio=productoServicio;
+    }
+
+    //mapear getUsuarios (Se puede mejorar con ResponseEntity
+    @GetMapping("/products")
+    public List <Producto> findAll(){return productoServicio.getProductos();}
+
+    @PostMapping ("/new-product/")
+    public ResponseEntity<Producto> saveUser (@RequestBody Producto newProduct){
+
+        //no se hace validacion si nos compran la pagina si
+        return  ResponseEntity.status(HttpStatus.CREATED)
+                .body(productoServicio.createProducto(newProduct));
+
+    }
 
 
 
