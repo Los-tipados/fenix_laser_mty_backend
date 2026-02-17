17750bd380c54f2650dc1e0543c3c10f4b4ee9fd
erik-perez-1 <erikp2913@gmail.com>
2026-02-17 15:29:42 -0600

    feat: se termino de realizar la clase ProductoServicio, para poder crear el controller

--- src/main/java/com/fenix_laser_mty/backend/servicios/ProductoServicio.java
+++ src/main/java/com/fenix_laser_mty/backend/servicios/ProductoServicio.java
@@ -15,46 +15,58 @@ public class ProductoServicio {
     private final ProductoRepository productoRepository;
 
     @Autowired
-    public  ProductoServicio(ProductoRepository productoRepository){this.productoRepository=productoRepository;}
+    public ProductoServicio(ProductoRepository productoRepository) {
+        this.productoRepository = productoRepository;
+    }
 
     //Metodo para recuperar todos los productos
-    public List <Producto> getProductos(){
+    public List<Producto> getProductos() {
         return productoRepository.findAll();
     }
 
     //Metodo para crear nuevas instancias
-    public Producto createProducto(Producto newProducto){
+    public Producto createProducto(Producto newProducto) {
         return productoRepository.save(newProducto);
     }
 
     //metodo abstracto a la realidad
-    public Producto findByNombre(String nombreProducto){
+    public Producto findByNombre(String nombreProducto) {
         return productoRepository.findByNombreProducto(nombreProducto);
 
     }
 
-    public Producto findById(Integer id ){
+    public Producto findById(Integer id) {
         return productoRepository.findById(id)
-                .orElseThrow(()-> new ProductoNotFoundException(id));
+                .orElseThrow(() -> new ProductoNotFoundException(id));
     }
 
-    public void deleteProducto (Integer id){
+    public void deleteProducto(Integer id) {
 
-        if (productoRepository.existsById(id)){
+        if (productoRepository.existsById(id)) {
             productoRepository.deleteById(id);
-        }else{
+        } else {
             throw new ProductoNotFoundException(id);
         }
 
     }
 
-//    public Producto updateProducto ( Producto producto, Integer id){
-//        return  productoRepository.findById(id)
-//                .map(productoData ->{
-//                    productoData.setPrecio(producto.getPrecio());
-//                })
-//
-//    }
+    public Producto updateProducto(Producto producto, Integer id) {
+        return productoRepository.findById(id)
+                .map(productoData -> {
+                    productoData.setNombre(producto.getNombre());
+                    productoData.setPrecio(producto.getPrecio());
+                    productoData.setImagen(producto.getImagen());
+                    productoData.setDescripcion(producto.getDescripcion());
+                    productoData.setCategoria(producto.getCategoria());
+                    productoData.setRating(producto.getRating());
+                    productoData.setEtiquetas(producto.getEtiquetas());
+                    productoData.setVisibleCatalogo(producto.getVisibleCatalogo());
+                    productoData.setVisibleRecomendados(producto.getVisibleRecomendados());
+
+                    return productoRepository.save(productoData);
+                }).orElseThrow(() -> new ProductoNotFoundException(id));
+
+    }
 
 
 
