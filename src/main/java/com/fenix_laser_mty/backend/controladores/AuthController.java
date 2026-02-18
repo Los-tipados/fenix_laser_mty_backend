c5c70834fc34ec9a75785217384de02ce7f2288e
erik-perez-1 <erikp2913@gmail.com>
2026-02-18 13:48:49 -0600

    feat: se crea la logica para AuthController

--- src/main/java/com/fenix_laser_mty/backend/controladores/AuthController.java
+++ src/main/java/com/fenix_laser_mty/backend/controladores/AuthController.java
@@ -1,16 +1,50 @@
 package com.fenix_laser_mty.backend.controladores;
 
 
+import com.fenix_laser_mty.backend.dto.LoginRequest;
+import com.fenix_laser_mty.backend.dto.LoginResponse;
+import com.fenix_laser_mty.backend.modelos.Usuario;
+import com.fenix_laser_mty.backend.servicios.UsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 @CrossOrigin(origins = "*")
 public class AuthController {
 
+@Autowired
+    private UsuarioServicio usuarioServicio ;
 
+@PostMapping("/login")
+    public ResponseEntity<?> login (@RequestBody LoginRequest loginRequest){
+
+    //metodo para buscar el correo
+    Usuario user = usuarioServicio.findByCorreo(loginRequest.getCorreo());
+
+    //en caso de que user sea null
+    if (user==null){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Error: El usuario no existe");
+    }
+
+    //validacion de password
+    if (!user.getPassword().equals(loginRequest.getPassword())){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body("Error: Credenciales incorrectas.");
+    }
+
+    LoginResponse response = new LoginResponse(
+            user.getIdUsuario(),
+            user.getNombre(),
+            user.getCorreo(),
+            user.getRol(),
+            true
+    );
+   return ResponseEntity.ok(response);
+}
 
 }
