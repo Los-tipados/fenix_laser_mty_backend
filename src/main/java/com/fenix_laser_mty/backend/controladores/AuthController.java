854c5d907b744f15ed66f0b2171f3dcf71d2c756
erik-perez-1 <erikp2913@gmail.com>
2026-02-18 13:38:01 -0600

    feat: se crea AuthController

---
+++ src/main/java/com/fenix_laser_mty/backend/controladores/AuthController.java
@@ -0,0 +1,16 @@
+package com.fenix_laser_mty.backend.controladores;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
+public class AuthController {
+
+
+
+}
