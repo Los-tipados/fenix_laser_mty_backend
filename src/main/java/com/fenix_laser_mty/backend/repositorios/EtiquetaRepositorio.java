687ba88631777d6bd82c59eb82612eba45e7cfd4
Ricardo Millán <ricardo.millan139@gmail.com>
2026-02-19 00:01:49 -0600

    Fix: se corrigen entidades para funcionamiento de endpoints

---
+++ src/main/java/com/fenix_laser_mty/backend/repositorios/EtiquetaRepositorio.java
@@ -0,0 +1,8 @@
+package com.fenix_laser_mty.backend.repositorios;
+
+import com.fenix_laser_mty.backend.modelos.Etiqueta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EtiquetaRepositorio extends JpaRepository<Etiqueta, Integer> {}
